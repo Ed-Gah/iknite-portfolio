6754fb6f746007092c6b11d8b6fa620fbc1a6bb7
Edwin Gah <88565289+Ed-Gah@users.noreply.github.com>
2023-07-29 00:29:54 +0100

    Update footer (#18)
    * Footer
    
    * Header Updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,28 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/images/logo.png";
 import Image from "next/image";
 import { Icons } from ".";
 import { IconType } from "@/types/icontypes/icon.type";
 import { MobileMenu } from "./MobileMenu";
-import { navItems } from "@/utils";
+import { navItems, navigateToPage } from "@/utils";
 import { NavItem } from "@/utils/constant/navItem";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
-  const [showNav, setShowNav] = useState(false);
+  const router = useRouter();
+  const [showNav, setShowNav] = useState<boolean>(false);
+  const [hover, setHover] = useState<boolean>(false);
+  const [hoverIndex, setHoverIndex] = useState(null) as any;
+  const [activeTitle, setActiveTitle] = useState<string>("Home");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const query: any = localStorage.getItem("@title");
+      setActiveTitle(query);
+    }
+  }, []);
+
   return (
     <div className="flex px-4 h-[88px] py-6 justify-between items-center max-w-[1200px] mx-auto ">
       <div className="flex">
@@ -19,12 +32,34 @@ export default function Navbar() {
         <div className="hidden w-full flex-grow md:flex lg:w-auto items-center lg:space-x-4">
           <ul className="flex items-center text-white justify-center font-medium flex-row gap-6 text-[16px]">
             {navItems.map((navItem: NavItem, i: number) => {
+              console.log("Active title : ", activeTitle);
+              console.log("Nave item : ", navItem.name);
               return (
-                <li key={i} className="block lg:inline-block">
+                <li
+                  key={i}
+                  // className="block lg:inline-block"
+                  className={
+                    hover && hoverIndex === i
+                      ? "animated bg-red-400 rounded-sm"
+                      : activeTitle === navItem.name
+                      ? "animated bg-red-400 rounded-sm"
+                      : " animated"
+                  }
+                  onClick={(e) => navigateToPage(e, navItem, router)}
+                  onMouseEnter={() => {
+                    setHover(true);
+                    setHoverIndex(i);
+                  }}
+                  onMouseLeave={() => setHover(false)}
+                >
                   {navItem.name === "About" ? (
-                    <Link href={"https://iknite.space"}>{navItem.name}</Link>
+                    <Link href={"https://iknite.space"} className="px-2 py-1">
+                      {navItem.name}
+                    </Link>
                   ) : (
-                    <Link href={navItem.href}>{navItem.name}</Link>
+                    <Link href={navItem.href} className="px-2 py-1">
+                      {navItem.name}
+                    </Link>
                   )}
                 </li>
               );
