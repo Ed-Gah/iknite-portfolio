df12f8062424dd539e784d3cd30d6d5280cdac38
Edwin Gah <88565289+Ed-Gah@users.noreply.github.com>
2023-07-28 17:40:51 +0100

    Layout (#4)
    * redux and reac-queries setup
    
    * Laayout setup
    
    * Layout modified

--- components/layout/layout.component.tsx
+++ components/layout/layout.component.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC } from "react";
+import React, { FC, Fragment } from "react";
 
 import { useRouter } from "next/navigation";
 
@@ -12,17 +12,17 @@ const Layout: FC<Props> = ({ children, crumbs }) => {
   const router = useRouter();
 
   return (
-    <div className=" flex bg-[var(--neutral-1)] w-full h-full">
-      <div>
+    <section className=" flex bg-[var(--neutral-1)] w-full h-full">
+      <Fragment>
         {/* Header here */}
-        <div className=" fixed top-0 z-[55] h-[88px]  w-full border-b border-[red] bg-[red] p-6">
-          <div></div>
-        </div>
+        <header className=" fixed top-0 z-[55] h-[88px]  w-full border-b border-[red] bg-[red] p-6">
+          <nav></nav>
+        </header>
         {/* Footer here */}
-        <div className=" fixed bottom-0 z-[55] h-[200px]  w-full border-b border-[var(--secondary-800)] bg-[green] p-6"></div>
-      </div>
-      <div className=" mt-32 px-6">{children}</div>
-    </div>
+        <footer className=" fixed bottom-0 z-[55] h-[200px]  w-full border-b border-[var(--secondary-800)] bg-[green] p-6"></footer>
+      </Fragment>
+      <section className=" mt-32 px-6">{children}</section>
+    </section>
   );
 };
 
