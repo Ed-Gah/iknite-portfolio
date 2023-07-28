dc360d64fa912a84d84cfb697f412a6d94b16cc1
Ed-Gah <edwingah2021@gmail.com>
2023-07-28 16:39:10 +0100

    redux and reac-queries setup

--- components/index.ts
+++ components/index.ts
@@ -1,36 +1,5 @@
 import Icons from "./icons/Icons";
-import Button, { ActionBtn } from "./buttons/button.component";
-import TitleBtn from "./header/header.component";
-import DocumentDropdown from "./document/documen_dropdown.component";
-import DocumentCreateHeader, {
-  ReviewBox,
-} from "./document/document_create_header";
-import TextArea from "./inputs/TextArea";
-import TextInput from "./inputs/TextInput.component";
-import DropdownField from "./inputs/DropdownField";
-import CreateDocumentModal from "./modals/create-document.modal";
-import ArticleList from "./article/list.articles";
+
 import Layout from "./layout/layout.component";
-import Search from "./search/search.component";
-import CreateArticleModal from "./modals/create-article.modal";
-import ConfirmMaterialModal from "./modals/save-confirm.modal";
-import LoadingError from "./loading-error/loadding-error";
-export {
-  Icons,
-  Button,
-  ActionBtn,
-  TitleBtn,
-  DocumentDropdown,
-  DocumentCreateHeader,
-  CreateArticleModal,
-  ReviewBox,
-  TextArea,
-  TextInput,
-  DropdownField,
-  CreateDocumentModal,
-  ArticleList,
-  Layout,
-  ConfirmMaterialModal,
-  Search,
-  LoadingError,
-};
+
+export { Icons, Layout };
--- utils/functions/data-manipulation.ts
+++
@@ -1,2 +0,0 @@
-export const filteredData = (data: any[], status: string) =>
-  data?.filter((doc: any) => doc.status === status);
