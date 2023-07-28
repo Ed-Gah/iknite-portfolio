8c5dc1c7dee793ab145e5e6b7abd66cf396f3854
Desmond51 <desmondtekum9@gmail.com>
2023-07-28 23:52:37 +0100

    Home Screens

---
+++ pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <div className="w-[1205px] text-center"><span className="text-purple-500 text-[56px] font-bold leading-[72px]">Hello, We’re Iknite Space,</span><span className="text-white text-[56px] font-bold leading-[72px]"> A creative<br/>start-up based in Cameroon</span></div>
+
+        <div className="w-[728px] text-center text-white text-lg font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamt.</div>
+
+        <div className="w-[1205px] h-36 justify-start items-end gap-[380px] inline-flex">
+    <div><span className="text-white text-[56px] font-bold leading-[72px]">Look at our<br/></span><span className="text-purple-500 text-[56px] font-bold leading-[72px]">Projects.</span></div>
+    <div className="w-[484px] text-white text-lg font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamt.</div>
+</div>
+    </div>
+  )
+}
--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "@/components";
 import React from "react";
+import Home from "../home/Home";
 
 const WelcomePage = () => {
   return (
     <Layout>
       <h2>Welcome screen</h2>
+      <Home />
     </Layout>
   );
 };
