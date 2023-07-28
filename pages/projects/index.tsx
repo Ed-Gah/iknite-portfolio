92590bdba096466c1fba89709afa2d7017378150
Casaemiro <silasmagho18@gmail.com>
2023-07-28 23:25:35 +0100

    added banners

---
+++ pages/projects/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "@/components";
+import { ProjectsBanner } from "@/components/ProjectsBanner";
+import React from "react";
+
+const WelcomePage = () => {
+  return (
+    <Layout>
+      <ProjectsBanner />
+    </Layout>
+  );
+};
+
+export default WelcomePage;
