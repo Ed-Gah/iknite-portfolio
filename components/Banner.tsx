c1823890c48b745711460609c32e8ac6e4598ecf
Desmond51 <desmondtekum9@gmail.com>
2023-07-29 01:38:13 +0100

    Working on home page

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,11 @@
+import Team from "./team/Page";
+
 export const Banner = () => {
   return (
     <div className="lg:w-[1000px] mt-[100px] py-[126px] flex flex-col gap-4">
       <div className="lg:text-[54px] text-center text-[25px] sm:text-[40px] text-white font-[700]">
         <span className=" text-transparent bg-clip-text bg-gradient-to-r from-haiti-300 to-pink-800">
-          Hello, We’re Iknite Space,
+          Hello, We’re Iknite Space, 
         </span>
         A creative start-up based in Cameroon
       </div>
@@ -12,6 +14,33 @@ export const Banner = () => {
         tempor incididunt
         <br /> ut labore et dolore magna aliqua. Ut enim ad minim veniamt.
       </div>
+      <div className="w-[1205px] h-36 justify-start items-end gap-[380px] mt-[126px] inline-flex">
+    <div>
+      <span className="text-white text-[56px] font-bold leading-[72px]">Look at our<br/></span><span className="text-purple-500 text-[56px] font-bold leading-[72px]">Projects.</span></div>
+    <div className="w-[484px] text-white text-lg font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamt.</div>
+</div>
+
+<div className="w-[1205px] text-center"><span className="text-white text-[56px] font-bold leading-[72px]">Meet Our </span><span className="text-purple-500 text-[56px] font-bold leading-[72px]">Team.</span></div>
+
+<div className="w-[738px] text-center text-white text-lg font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamt.</div>
+
+<div>
+<div className="w-[164px] h-[46px] px-6 py-2 rounded-[32px] border border-purple-500 justify-start items-start inline-flex">
+    <div className="text-white text-xl font-bold">Developers</div>
+</div>
+<div className="text-white text-xl font-bold">Designers</div>
+</div>
+<div className="grid grid-cols-12 gap-[100px]">
+  <div className="col-span-4">
+    <Team />
+  </div>
+  <div className="col-span-4">
+    <Team />
+  </div>
+  <div className="col-span-4">
+    <Team />
+  </div>
+</div>
     </div>
   );
 };
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,13 +1,13 @@
-import { Layout } from "@/components";
-import { Banner } from "@/components/Banner";
-import React from "react";
+import { Layout } from "@/components"
+import { Banner } from "@/components/Banner"
+import React from "react"
 
 const WelcomePage = () => {
   return (
     <Layout>
       <Banner />
     </Layout>
-  );
-};
+  )
+}
 
-export default WelcomePage;
+export default WelcomePage
