b362dc5b8f740ad4f517a8702e87b01b622b0ad1
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-07-29 01:07:52 +0100

    Adjusted imports

--- components/index.ts
+++ components/index.ts
@@ -1,13 +1,15 @@
 import Icons from "./icons/Icons";
-
 import Layout from "./layout/layout.component";
 import TeamHeader from "./team.header.component/Team.header.component";
 import TeamMembers from "./team.member.component/Team.member.component";
 import MemberCard from "./memberCard/MemberCard";
 import Banner from "./Banner/Banner";
 import Navbar from "./Navbar/Navbar";
+import EventsBanner from "./eventBanner/EventsBanner";
+import ProjectCard from "./ProjectCard/ProjectCard";
+import ProjectsBanner from "./ProjectsBanner/ProjectsBanner";
 import { Cards } from "./cards/Cards";
 
 import Footer from "./footer/footer.components";
 
-export { Icons,Cards, Layout, TeamHeader, TeamMembers, MemberCard, Banner, Navbar, Footer };
+export { Icons, Layout, TeamHeader, TeamMembers, MemberCard, Banner, Navbar, Footer };
