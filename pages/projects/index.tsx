8165f5dc7980b6091512303527d980cdf3d021d5
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-07-29 09:10:34 +0100

    Updated layout of project and event page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,57 +1,14 @@
-import { Cards, Layout, ProjectsBanner } from "@/components";
+import { Cards, Layout, ProjectMainComponent, ProjectsBanner } from "@/components";
 import React from "react";
-import image from "../../assets/images/card.png";
-import { useGetProjectsData } from "@/query";
+
 
 const Projects = () => {
-  /**
-   * Methods
-   */
-  const onSuccess = (data: any) => {};
-
-  const onError = (error: any) => {
-    console.log("Perform sid effect after error fecthing :", error);
-  };
-  const { isLoading, isError, data, error, isFetching, refetch } =
-    useGetProjectsData(onSuccess, onError) as any;
-  console.log("Datatatatata :", { data });
-
-  if (isLoading) {
-    return (
-      <Layout>
-        <div className="mt-32 text-white">
-          <h2>Projects loading.....</h2>
-        </div>
-      </Layout>
-    );
-  }
-
-  if (isError) {
-    return (
-      <Layout>
-        <div className="mt-32 text-white">
-          <h2>{error?.message}</h2>
-        </div>
-      </Layout>
-    );
-  }
+  
   return (
     <Layout>
       <div className="max-w-[1200px] flex flex-col items-center text-white">
         <ProjectsBanner />
-        <section className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-10">
-          {data?.data?.data.map((data: any, i: number) => {
-            return (
-              <Cards
-                key={i}
-                id={data._id}
-                title={data.title}
-                image={image}
-                details={data?.details}
-              />
-            );
-          })}
-        </section>
+        <ProjectMainComponent />
       </div>
     </Layout>
   );
