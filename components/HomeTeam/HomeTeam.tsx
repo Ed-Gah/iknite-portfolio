4502e4cb1552991784f437269dad65926f356881
Casaemiro <silasmagho18@gmail.com>
2023-07-29 08:48:32 +0100

    done

--- components/HomeTeam/HomeTeam.tsx
+++ components/HomeTeam/HomeTeam.tsx
@@ -4,7 +4,7 @@ import arrow_right from "../../assets/images/arrow_right.svg";
 
 const HomeTeam = () => {
   return (
-    <div className="lg:w-[1000px] pt-[60px] flex flex-col gap-4">
+    <div className="lg:w-[1000px] pt-[100px] flex flex-col gap-4">
       <div className="lg:text-[54px] text-center text-[25px] sm:text-[40px] text-white font-[700]">
         Meet Our {" "}
         <span className=" text-transparent bg-clip-text bg-gradient-to-r from-haiti-300 to-pink-800">
