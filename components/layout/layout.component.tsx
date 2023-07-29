de2fa5ed58f79ec81f89473f08897cff56b4e990
Mbianou Bradon <mbianoubradon2000@gmail.com>
2023-07-29 02:12:52 +0100

    Adjust team page layout (#25)

--- components/layout/layout.component.tsx
+++ components/layout/layout.component.tsx
@@ -21,7 +21,7 @@ const Layout: FC<Props> = ({ children, crumbs }) => {
         </header>
         <div>
           {/* Section */}
-          <section className="px-28 flex flex-col items-center">{children}</section>
+          <section className="flex flex-col items-center">{children}</section>
           {/* Footer here */}
           <footer className=" bg-[#161513] h-[200px] w-screen pt-6 pb-14 px-16">
             <Footer />
