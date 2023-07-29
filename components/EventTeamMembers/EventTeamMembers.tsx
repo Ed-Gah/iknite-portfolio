f507d88cea6beeee776deee311174518bdbbff41
Akoneh Silas Magho <silasmagho18@gmail.com>
2023-07-29 12:21:00 +0100

    improvements (#49)

---
+++ components/EventTeamMembers/EventTeamMembers.tsx
@@ -0,0 +1,20 @@
+import { TeamCard } from "..";
+import image from "../../assets/images/card.png";
+
+const EventTeamMembers = () => {
+  return (
+    <div className="w-full text-white">
+      <div className="md:text-[30px] text-[20px] font-[700] my-[30px]">
+        Different teams and members who participated
+      </div>
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+        <TeamCard image={image} title={"Beta"} details={"dfk"} />
+        <TeamCard image={image} title={"Beta"} details={"dfk"} />
+        <TeamCard image={image} title={"Beta"} details={"dfk"} />
+        <TeamCard image={image} title={"Beta"} details={"dfk"} />
+      </div>
+    </div>
+  );
+};
+
+export default EventTeamMembers;
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -3,8 +3,23 @@ import { EventsBanner,  } from "@/components";
 import image from "../../assets/images/card.png";
 import React from "react";
 
-
 const Events = () => {
+  const onSuccess = (data: any) => {};
+  const onError = (error: any) => {
+    console.log("Perform sid effect after error fetching :", error);
+  };
+  const { isLoading, isError, data, error, isFetching, refetch } =
+    useGetEventsData(onSuccess, onError) as any;
+  console.log("Datatatatata :", { data });
+  if (isLoading) {
+    return (
+      <Layout>
+        <div className="mt-32 text-white">
+          <h2>Events loading.....</h2>
+        </div>
+      </Layout>
+    );
+  }
 
   return (
     <Layout>
