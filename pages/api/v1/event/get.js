59baafa6d97b445f8c97e84f59a7d0b5c588b64c
Asonganyi Rouclec <81648176+Rouclec@users.noreply.github.com>
2023-07-29 03:18:41 +0100

    Api setup (#33)
    * api setup
    
    * 80% api
    
    * all populates working
    
    * all populate working

--- pages/api/v1/event/get.js
+++ pages/api/v1/event/get.js
@@ -2,5 +2,18 @@ import { getOne } from "../../lib/helper"
 import Event from "../../models/Event"
 
 
-const getEvent = getOne(Event, 'projects')
+const getEvent = getOne(Event, [{
+    path: 'projects',
+    populate: [{
+        path: 'projectManager',
+        model: 'Member',
+    }, {
+        path: 'contributors',
+        model: 'Contributor',
+        populate: {
+            path: 'member',
+            model: 'Member'
+        }
+    }]
+}])
 export default getEvent
