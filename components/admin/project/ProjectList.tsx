9cc0f44bd8a133455d4f483db9bd36f1c6b6e220
Tambua Evaristus <evaristustambua@gmail.com>
2023-07-29 05:59:48 +0100

    dark theme on admin dashboard

--- components/admin/project/ProjectList.tsx
+++ components/admin/project/ProjectList.tsx
@@ -14,7 +14,7 @@ export default function ProjectList() {
         <div className="w-full overflow-x-auto">
           <table className="whitespace-no-wrap w-full">
             _
-            <tr className="border-b bg-gray-50 text-left text-xs font-semibold uppercase tracking-wide text-gray-500 dark:bg-gray-800 dark:text-gray-400">
+            <tr className="border-b text-white text-left text-xs font-semibold uppercase tracking-wide text-gray-500 bg-gray-800 dark:text-gray-400">
               <th className="px-4 py-3">Name</th>
               <th className="px-4 py-3">Description</th>
               <th className="px-4 py-3">Actions</th>
