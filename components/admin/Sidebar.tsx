2d578daf1f5e8c0a5efb9b7127d00d421a1c5772
Tambua Evaristus <44694224+tambuaevaristus@users.noreply.github.com>
2023-07-29 08:53:21 +0100

    Admin UI (#42)
    * improvements on the home page
    
    * dark theme on admin dashboard
    
    * merged
    
    ---------
    
    Co-authored-by: Casaemiro <silasmagho18@gmail.com>

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Link from "next/link";
 
-
 export default function Sidebar({ isNavOpen }: any) {
-  
   return (
     <div className=" ">
       <div className={isNavOpen ? "block lg:block" : "hidden lg:block"}>
-        <div className=" left-0 z-20 h-screen overflow-y-auto px-5 md:block">
+        <div className=" left-0 z-20 h-screen  overflow-y-auto px-5 md:block">
           <div className="py-4 text-gray-500 ">
             <ul className="">
               <li className="relative px-6 py-3">
                 <Link
-                  className="inline-flex w-full items-center text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 "
+                  className="inline-flex w-full  items-center text-sm font-semibold text-white transition-colors duration-150 "
                   href="/admin"
                 >
                   <span className="ml-4">Dashboard</span>
@@ -22,7 +20,7 @@ export default function Sidebar({ isNavOpen }: any) {
             <ul>
               <li className="relative px-6 py-3">
                 <Link
-                  className="inline-flex w-full items-center text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+                  className="inline-flex w-full items-center text-sm font-semibold transition-colors duration-150 text-white"
                   href="/admin/project"
                 >
                   <span className="ml-4">Projects</span>
@@ -30,8 +28,8 @@ export default function Sidebar({ isNavOpen }: any) {
               </li>
 
               <li className="relative px-6 py-3">
-              <Link
-                  className="inline-flex w-full items-center text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+                <Link
+                  className="inline-flex w-full items-center text-sm font-semibold transition-colors duration-150 text-white"
                   href="/admin/team"
                 >
                   <span className="ml-4">Team</span>
@@ -40,7 +38,7 @@ export default function Sidebar({ isNavOpen }: any) {
 
               <li className="relative px-6 py-3">
                 <Link
-                  className="inline-flex w-full items-center text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+                  className="inline-flex w-full items-center text-sm font-semibold transition-colors duration-150 text-white"
                   href=""
                 >
                   <span className="ml-4">Logout</span>
