df9ca899c871669c1a4fe5570c7528fc52727cab
Ed-Gah <edwingah2021@gmail.com>
2023-07-31 08:11:19 +0100

    Deployment

--- pages/events/[id]/index.tsx
+++ pages/events/[id]/index.tsx
@@ -1,9 +1,15 @@
-import { EventDescription, EventTeamMembers, Layout, WinningTeam } from "@/components";
+import {
+  EventDescription,
+  EventTeamMembers,
+  Layout,
+  WinningTeam,
+} from "@/components";
 import React from "react";
 import { useRouter } from "next/router";
 
 const EventDetails = () => {
   const { id } = useRouter().query;
+  console.log("Id: ", id);
   return (
     <Layout>
       <div className=" mt-24 pb-5 text-transparent bg-clip-text bg-gradient-to-r from-haiti-300 to-pink-800 lg:text-[40px] text-[25px] sm:text-[30px] font-[700]">
